57d9a7965aab555ad05015be8a62c7eda49adb66
Natee Rudkui <nateerudkui@outlook.com>
2023-10-24 09:03:04 +0700

    ✅ : ขึ้นโครง

---
+++ src/components/FormComponent.js
@@ -0,0 +1,36 @@
+const FormComponent = () => {
+  return (
+    <div className="container">
+      <form className="form">
+        <div className="form-control">
+          <label>ชื่อผ้ใช้</label>
+          <input type="text"></input>
+          <small>error magssge</small>
+        </div>
+      </form>
+      <form className="form">
+        <div className="form-control">
+          <label>email</label>
+          <input type="text"></input>
+          <small>error magssge</small>
+        </div>
+      </form>
+      <form className="form">
+        <div className="form-control">
+          <label>password</label>
+          <input type="text"></input>
+          <small>error magssge</small>
+        </div>
+      </form>
+      <form className="form">
+        <div className="form-control">
+          <label>ยืนยัน password</label>
+          <input type="text"></input>
+          <small>error magssge</small>
+        </div>
+      </form>
+      <button type="submit"></button>
+    </div>
+  );
+};
+export default FormComponent;

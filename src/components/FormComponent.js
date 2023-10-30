f52fcf4bb9d3c04969ca203e07be3f911b283a1c
Natee Rudkui <nateerudkui@outlook.com>
2023-10-30 08:56:37 +0700

    ✅ : แจ้งเตือน error เมื่อกรอกข้อมูลไม่ถุกต้อง

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -12,19 +12,27 @@ const FormComponent = () => {
   const [errorPassword, setErrorPassword] = useState("password ไม่ปลอดภัย");
   const [errorRePassword, setErrorRePassword] = useState("รหัสผ่านไม่ตรงกัน");
 
+  const [userNameColor, serUserNameColor] = useState("");
+  const [emailColor, setEmailColor] = useState("");
+  const [passwordColor, setPasswordColor] = useState("");
+  const [rePasswordColor, setRePasswordColor] = useState("");
+
   const validateForm = (e) => {
     e.preventDefault();
-
     if (userName.length > 8) {
       setErrorUserName("");
+      serUserNameColor("green");
     } else {
       setErrorUserName("username ต้องมีความยา 8 ตัวอักษรขึ้นไป");
+      serUserNameColor("red");
     }
 
     if (email.includes("@")) {
       setErrorEmail("");
+      setEmailColor("green");
     } else {
       setErrorEmail("รูปแบบ email ไม่ถูกต้อง");
+      setEmailColor("red");
     }
   };
   return (
@@ -36,8 +44,9 @@ const FormComponent = () => {
             type="text"
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
+            style={{ borderColor: userNameColor }}
           ></input>
-          <small>{errorUserName}</small>
+          <small style={{ color: userNameColor }}>{errorUserName}</small>
         </div>
         <div className="form-control">
           <label>email</label>
@@ -45,13 +54,14 @@ const FormComponent = () => {
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            style={{ borderColor: emailColor }}
           ></input>
-          <small>{errorEmail}</small>
+          <small style={{ color: emailColor }}>{errorEmail}</small>
         </div>
         <div className="form-control">
           <label>password</label>
           <input
-            type="text"
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></input>
@@ -60,13 +70,13 @@ const FormComponent = () => {
         <div className="form-control">
           <label>ยืนยัน password</label>
           <input
-            type="text"
+            type="password"
             value={repassWord}
             onChange={(e) => setRePassword(e.target.value)}
           ></input>
           <small>{errorRePassword}</small>
         </div>
-        <button type="submitฃ">Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
